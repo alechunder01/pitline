9dcd753e29d632dec29ac19930b6de0e6211a02b
alechunder <61816nicolas@student.sstebrno.cz>
2025-05-21 08:34:40 +0200

    functioning menu

--- scripts/menuopener.js
+++ scripts/menuopener.js
@@ -1,11 +1,17 @@
 function openMenu() {
+  const menuButton = document.getElementById("menubutton");
   const menu = document.getElementById("menu");
 
   if (menu.classList.contains("closed")) {
     menu.classList.remove("closed");
     menu.classList.add("open");
+    menuButton.classList.add("white");
+    menuButton.classList.remove("black");
+
   } else {
     menu.classList.remove("open");
     menu.classList.add("closed");
+    menuButton.classList.add("black");
+    menuButton.classList.remove("white");
   }
 }
